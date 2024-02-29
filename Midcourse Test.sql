f2fc374f9a51e63ce80135a30a425242b7b35a7f
rileyriles <160503137+rileyriles@users.noreply.github.com>
2024-03-01 00:20:39 +0700

    Midcourse Test.sql

--- Midcourse Test.sql
+++ Midcourse Test.sql
@@ -42,4 +42,30 @@ INNER JOIN film as c ON b.film_id=c.film_id
 GROUP BY a.first_name, a.last_name
 ORDER BY COUNT(b.film_id) DESC
 
+---- QUESTION 6
+SELECT COUNT(a.address_id) FILTER(WHERE b.address_id is null)
+FROM address as a
+LEFT JOIN customer as b
+ON a.address_id=b.address_id
+
+---- QUESTION 7
+SELECT a.city, SUM(d.amount)
+FROM city as a
+INNER JOIN address as b ON a.city_id=b.city_id
+INNER JOIN customer as c ON b.address_id = c.address_id
+INNER JOIN payment as d ON c.customer_id=d.customer_id
+GROUP BY a.city
+ORDER BY SUM(d.amount) DESC
+
+---- QUESTION 8
+SELECT a.country || ',' || ' ' || b.city as Country_and_City, SUM(e.amount) as total_amount
+FROM country as a
+INNER JOIN city as b ON a.country_id=b.country_id
+INNER JOIN address as c ON b.city_id=c.city_id
+INNER JOIN customer as d ON c.address_id = d.address_id
+INNER JOIN payment as e ON d.customer_id=e.customer_id
+GROUP BY a.country || ',' || ' ' || b.city
+ORDER BY SUM(e.amount) DESC
+
+--- NOTE: Question 8 input answer từ đề bài có sai không? Bởi vì đề hỏi doanh thu cao nhất, tức United States, Cape Coral với 221.55 amount. Còn United States, Tallahassee should be nhỏ nhất, với 50.85 total_amount
 
